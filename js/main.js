31b9119b637e6f5a75e7e158a5aad8f382102b18
godsaveme <javierjesusalvarezmontenegro@gmail.com>
2018-04-08 19:41:19 -0500

    feat(main.js): Add calendarDataCalculation function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-(function () {
+(function (m) {
     'use strict';
 
     var calendarForm = document.getElementById("calendarForm"),
+        startDate = document.getElementById('startDate'),
+        daysNumber = document.getElementById('daysNumber'),
+        momentStartDate,
+        momentEndDate,
+        years,
+        months,
         calendarFormSubmit = function (event) {
             event.preventDefault();
+            calendarDataCalculation();
         };
-
+        
     calendarForm.addEventListener("submit", calendarFormSubmit);
-})();
+
+    function calendarDataCalculation() {
+        momentStartDate = m(startDate.value);
+        momentEndDate = m(startDate.value).add(daysNumber.value, 'd');
+        years = momentEndDate.year() - momentStartDate.year() + 1;
+        months = momentEndDate.month() - momentStartDate.month() + 1;
+    }
+})(moment);
